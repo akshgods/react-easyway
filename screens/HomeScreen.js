3ad02db9ebc4b7534608f7f381db62bec904ce6d
akshgods <akshgods@gmail.com>
2018-09-22 16:32:41 +0530

    updated home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,9 +6,10 @@ import {
     TouchableOpacity,
     AsyncStorage,
     Image,
-    Dimensions
+    Dimensions,
+    Button
 } from "react-native";
-import { Button,Icon } from "native-base";
+import { Icon } from "native-base";
 
 const screenSize={
     height:Dimensions.get('window').height,
@@ -20,49 +21,34 @@ class HomeScreen extends Component {
         await AsyncStorage.removeItem('isLogin');
         this.props.navigation.navigate('AuthLoding');
     }
+    catlogPage=()=>{
+        this.props.navigation.navigate('Catlog');
+    }
     render() {
         return (
             <View style={styles.container}>
-            <View style={{flex:3,backgroundColor:'pink',}}>
-                <View style={{height:(screenSize.height/3.5)}}>
-                    <Image 
-                    style={{width:(screenSize.width/1.3),height:(screenSize.height/3),
-                            left:30,
-                            top:-20,zIndex:1,position:'absolute'}}
-                     resizeMode={'contain'}
-                    source={require('../assets/imgs/BG_full_home.png')} />
-                        <Image style={{width:200,height:150,position:'absolute',zIndex:2}}  source={require('../assets/imgs/Genba-Package001.png')} /> 
-                        <Image  style={{width:200,height:100,position:'absolute',zIndex:3}} source={require('../assets/imgs/Plant_home.png')} />          
-                    <Button style={{position:'absolute',top:25,right:30}} rounded onPress={()=>{this.props.navigation.navigate('Catlog');}}>
-                        <Text>Only see catalog</Text>
-                    </Button>
+                <View style={{flex:3,backgroundColor:'red',minHeight:300}}>                  
+                    <TouchableOpacity style={{backgroundColor:'blue',padding:10
+                ,borderRadius:20,alignSelf: 'baseline' ,justifyContent: "center",flexDirection:"row",alignItems: "center"}}
+                onPress={this.catlogPage}
+                activeOpacity={0.8}
+                >
+                        <Text style={{fontSize:14,color:'white'}} allowFontScaling
+                        adjustsFontSizeToFit={true}
+                        minimumFontScale={.5}
+                        numberOfLines={1}
+                        >Catlog Page hello world</Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={{height:(screenSize.height/4),zIndex:0,backgroundColor:'green'}}>
-                    <Button iconLeft transparent primary>
-                        <Icon name='arrow-forward' />
-                    </Button>
-                    <Text>Realize an order for me
-                        <Text>
-                            ask for today or schedule your order
-                        </Text>
-                    </Text>
+                <View style={{flex:2,backgroundColor:'green',flexDirection:'row',minHeight:150}}>
+                    <View style={{flex:3,backgroundColor:'pink'}}>
+
+                    </View>
+                    <View style={{flex:2,backgroundColor:'blue'}}>
+                        
+                        </View>
                 </View>
-           
-            </View>
-            
-            <View style={{flex:2,backgroundColor:'green',flexDirection:'row'}}>
-                <View style={{flex:1,backgroundColor:'blue'}}>
-                    <Text>Send as a gift</Text>
-                </View>
-                <View style={{flex:1,backgroundColor:'red'}}>
-                    <Text>Mi cuenta</Text>
-                    <TouchableOpacity onPress={this.logoutMe}>
-                <Text>Logout</Text>
-            </TouchableOpacity>
-                </View>
-            </View>
-    
-        </View>
+             </View>
         );
     }
 }
@@ -71,7 +57,6 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+      
     }
 });
